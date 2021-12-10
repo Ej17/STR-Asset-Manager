924fb3cc4fe17a91c5dc9d4edd51995c02fd4eff
EJ <ejralph67@gmail.com>
2021-12-10 10:13:08 +0000

    Code has been added to popup.js so that a PUT request can be made to local server to send device information. This will then be handled my PHP file which will save data to SQL database.

--- Chrome_info_ext/popup.js
+++ Chrome_info_ext/popup.js
@@ -9,9 +9,12 @@ function addToTable(rows) {
 		cell2.innerHTML = rows[index];
    }
 }
-    
+
 document.onreadystatechange = function () {
     if (document.readyState === "interactive") {
+		var serial_num = '';
+		var asset_id = '';
+		var email = '';
 		chrome.storage.managed.get(['primary_domain', 'fields', 'remote_url'], function(values) {
 			primary_domain = values.primary_domain;
 			if (typeof values.primary_domain == 'undefined') {
@@ -34,17 +37,14 @@ document.onreadystatechange = function () {
 				switch ( item ) {
 					case 'serialNumber':
 						chrome.enterprise.deviceAttributes.getDeviceSerialNumber(function(sn) {
+						serial_num = sn;	
 						addToTable({'Serial Number': sn});
 					});
 					break;
 					case 'annotatedAssetId':
 						chrome.enterprise.deviceAttributes.getDeviceAssetId(function(aid) {
+						asset_id = aid;	
 						addToTable({'Asset ID': aid});
-					});
-					break;
-					case 'annotatedLocation':
-						chrome.enterprise.deviceAttributes.getDeviceAnnotatedLocation(function(loc) {
-						addToTable({'Location': loc});
 					});
 					break;
 					default:
@@ -53,10 +53,35 @@ document.onreadystatechange = function () {
 			}); 
 				
 		});
+
+		if (document.readyState === "interactive") {
+			chrome.identity.getProfileUserInfo({'accountStatus': 'ANY'}, function(info) {
+				email = info.email;
+				addToTable({'User': email});
+			});
+		}
 		
-		chrome.identity.getProfileUserInfo({'accountStatus': 'ANY'}, function(info) {
-			var email = info.email;
-			addToTable({'User': email});
-		});
+		callPHP(email, serial_num, asset_id);
+	}	
+
+	function callPHP(email, serial_num, asset_id) {
+
+		// Set up an asynchronous AJAX POST request
+		var xhr = new XMLHttpRequest();
+		var data = encodeURIComponent(email+serial_num+asset_id);
+		//var params = 'user=' + user;
+		// The URL to POST our data to
+		var postUrl = 'http://str-webdev-svr/Example/test3.php';
+		
+		xhr.open('POST', postUrl, true);
+		// Send the request and set status
+		xhr.send(data);
+
+		// Handle request state change events
+		xhr.onreadystatechange = function() { 
+			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+				console.log("Got response 200!");
+			}
+		};
 	}
 };
