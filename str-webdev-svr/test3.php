002fefd42c11f2faef18150eae444dadbb36246b
EJ <39485922+Ej17@users.noreply.github.com>
2022-07-26 16:07:16 +0100

    Add files via upload
    PHP code accepts input from chrome extensions, sent using REST web services, and will create a new row in the table created using SQL.

--- str-webdev-svr/test3.php
+++ str-webdev-svr/test3.php
@@ -4,19 +4,27 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-header('Content-Type: text/plain');
-$_POST = urldecode(file_get_contents("php://input"));
+header('Content-Type: application/json');
+$post_data = json_decode(urldecode(file_get_contents("php://input")));
+
+$user = $post_data->items[0]->User;
+//$asset = $post_data->items[1]->Asset;
+//$serial = $post_data->items[2]->Serial;
+
 
 $servername = "127.0.0.1";
 $username = "admin";
 $password = "strassetadmin!";
 $dbname = "test";
 
-// Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+// $sql = "INSERT INTO deviceUsers (user, serial, assetID)
+// VALUES ('$user', '$asset', '$serial')";
+
 $sql = "INSERT INTO deviceUsers (user)
-VALUES ('$_POST')";
+VALUES ('$user')";
 
 if (mysqli_query($conn, $sql)) {
   echo "New record created successfully";
